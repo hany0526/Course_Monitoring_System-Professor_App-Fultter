21d8bf45816ddab8d39022e150b677bc97edecca
unknown <alrsamkarim96@gmail.com>
2019-12-13 17:32:57 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/course_monitoring_system_professor_app_fultter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.course_monitoring_system_professor_app_fultter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
